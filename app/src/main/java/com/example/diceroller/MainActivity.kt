9b554a61189f727c05149835fdcd02d36e894702
Rommy Soeli <rommysoeli@gmail.com>
2023-03-12 22:36:50 +0700

    Dice roller app but with image, this version still hardcoded the result as 2

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -19,10 +19,8 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        // find the id of the textView so the next line could put value to it
-        val resultTextView: TextView = findViewById(R.id.textView)
-        // put the result of converted string to the id resolved by the prev line
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 
